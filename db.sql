99d464d6d676318fc5f878becc4a79cfab8e7fe2
Joel <joel02varghese@gmail.com>
2024-02-12 00:01:26 +0530

    Add device registration schema

--- db.sql
+++ db.sql
@@ -9,3 +9,9 @@ CREATE TABLE main.users (
   salt BYTEA NOT NULL,
   join_date TIMESTAMP NOT NULL
 );
+
+CREATE TABLE main.user_devices (
+  user_id uuid REFERENCES main.users(id) ON DELETE CASCADE,
+  token TEXT UNIQUE NOT NULL,
+  device_name VARCHAR(255) NOT NULL
+);
