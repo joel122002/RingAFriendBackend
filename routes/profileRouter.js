8699ada0beea57ce3aaca6c221056f4e6ebfa162
Joel <joel02varghese@gmail.com>
2024-03-05 02:01:12 +0530

    Replace tabs with spaces

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -9,46 +9,44 @@ import client from '#root/db.js';
 const profileRouter = express.Router();
 
 profileRouter.get('/profile', isAuthenticated, async function (req, res) {
-	const response = await client.query(
-		'SELECT * FROM main.users WHERE id = $1::uuid;',
-		[req.user.id]
-	);
-	const row = response.rows[0];
-	return res.send({
-		username: row.username,
-		join_date: row.join_date,
-	});
+    const response = await client.query(
+        'SELECT * FROM main.users WHERE id = $1::uuid;',
+        [req.user.id]
+    );
+    const row = response.rows[0];
+    return res.send({
+        username: row.username,
+        join_date: row.join_date,
+    });
 });
 
 profileRouter.get('/send-to-user/:username', function (req, res) {
-	const username = req.params.username;
-	const message = {
-		data: {
-			message: '',
-		},
-		topic: `${username}`,
-		android: {
-			priority: 'high'
-		}
-	};
-	console.log(message);
-	getMessaging()
-		.send(message)
-		.then((response) => {
-			res.sendStatus(204);
-		})
-		.catch((error) => {
-			res.status(500);
-			return res.send({
-				error: `Error sending message: ${error}`,
-			});
-		});
+    const username = req.params.username;
+    const message = {
+        data: { username: username },
+        topic: `${username}`,
+        android: {
+            priority: 'high',
+        },
+    };
+    console.log(message);
+    getMessaging()
+        .send(message)
+        .then((response) => {
+            res.sendStatus(204);
+        })
+        .catch((error) => {
+            res.status(500);
+            return res.send({
+                error: `Error sending message: ${error}`,
+            });
+        });
 });
 
 profileRouter.get('/get-all-users', isAuthenticated, async function (req, res) {
-	const response = await client.query('SELECT username FROM main.users;');
-	const row = response.rows;
-	return res.send(row);
+    const response = await client.query('SELECT username FROM main.users;');
+    const row = response.rows;
+    return res.send(row);
 });
 
 export { profileRouter };
